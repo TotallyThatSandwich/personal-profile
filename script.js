2175f10e8edee0e471c50f947da8deaa7fa62277
TapTrap2090 <hamishHeath2008@gmail.com>
2023-03-20 14:57:41 +1100

    n

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var controller = new ScrollMagic.Controller();
+
+var Scene = new ScrollMagic.Scene({
+    triggerElement: '#fade'
+})
+
+
+.setClassToggle('#fade', 'fade-in')
+.addTo(controller)
